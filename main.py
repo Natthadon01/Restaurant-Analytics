045bdbd72a5f6d3c03e812b759efe35481b19068
Natthadon01 <155519494+Natthadon01@users.noreply.github.com>
2024-04-04 13:31:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.title('Restaurant Analytics')
 
 
 
-## Chart 1 Food Sales trend
+## Chart 1 Trend of Food Products Sales
 
 # Adjust datatype and format of the date column.
 df[['Day', 'Month', 'Year']] = df['Date'].str.split('/', expand=True)
@@ -31,7 +31,7 @@ df["Month Name"] = df["Date"].dt.month_name()\
                              .upper())
 
 
-# Count the number of orders grouped by Month Name and Menu.
+# Count the number of food orders grouped by Month Name and Menu.
 ftrend = df[df["Category"] == "food"]\
     .groupby(["Month Name","Menu"])["Price"]\
     .agg("count")\
@@ -49,7 +49,7 @@ ftrend["Month Name"] = pd.Categorical(ftrend["Month Name"], categories=Month, or
 ftrend.sort_values("Month Name", inplace=True)
 
 
-# Create Chart 1: Food Sales Trend.
+# Create Chart 1
 chart1 = px.line(ftrend, 
                     x='Month Name', 
                     y='Quantity', 
@@ -57,71 +57,76 @@ chart1 = px.line(ftrend,
                     title= 'Trend of Food Products Sales')
 
 chart1.update_layout(xaxis_title='', 
-                     yaxis_title='Quantity')
+                     yaxis_title='Quantity',
+                     yaxis=dict(range=[0, max(ftrend['Quantity'])+300]))
 
 
+## Chart 2 Trend of Beverage Product Sales
 
+# Count the number of drink orders grouped by Month Name and Menu.
+dtrend = df[df["Category"] == "drink"]\
+        .groupby(["Month Name","Menu"])["Price"]\
+        .agg("count").reset_index()
 
-## Chart 2 Drink trend
-dtrend = df[df["Category"] == "drink"].groupby(["Month Name","Menu"])["Price"]\
-                                      .agg("count")\
-                                      .reset_index()
-
+# Sort the data by month.
 dtrend["Month Name"] = pd.Categorical(dtrend["Month Name"], categories=Month, ordered=True)
 
 dtrend = dtrend.rename(columns= {"Price":"Quantity"})
 
-#เรียงข้อมูล
 dtrend.sort_values("Month Name", inplace=True)
 
 
-# Create line chart using Plotly Express
+# Create Chart 2
 chart2 = px.line(dtrend, 
                     x='Month Name', 
                     y='Quantity',
                     color='Menu', 
                     title='Trend of Beverage Products Sales')
 
-# Add axis titles
-chart2.update_layout(xaxis_title='',
-                         yaxis_title='Quantity')
-
-# Display the line chart using Streamlit
+chart2.update_layout(xaxis_title='',yaxis_title='Quantity',
+                     yaxis=dict(range=[0, max(dtrend['Quantity'])+100]))
 
 
+##Chart 3 Food Product Sales
 
+# Summarize Total Sales.
+chart3 = df.query("Category == 'food'")\
+        .groupby("Menu")["Price"]\
+        .agg("sum").round().reset_index()
 
-##Chart 3 Food Barchart
-#filter data to visualize
-chart3 = df.query("Category == 'food'").groupby("Menu")["Price"].agg("sum").round().reset_index()
+# Sort the data.
 chart3_data_sort = chart3.sort_values(by='Price', ascending=True)
+
+# Adjust data label formatting.
 chart3_data_sort["Price_T"] = (chart3_data_sort["Price"]/1000).round(decimals = 1).astype(str) + "K"
 
-#setting Chart
-chart3_plot = px.bar(chart3_data_sort, x='Price', y='Menu', orientation='h', 
-    title='Food Products Sales', text = 'Price_T')
+
+# Create Chart 3
+chart3_plot = px.bar(chart3_data_sort, 
+                     x='Price', 
+                     y='Menu', 
+                     orientation='h', 
+                     title='Food Products Sales', 
+                     text = 'Price_T')
 
 chart3_plot.update_layout(yaxis_title='', xaxis_title = 'Sales')
 
-#plot horizontal bar chart
 
+##Chart 4 Beverage Products Sales
 
+# Summarize Total Sales.
+chart4 = df.query("Category == 'drink'")\
+           .groupby("Menu")["Price"]\
+           .agg("sum").round().reset_index()
 
-
-
-
-
-
-
-
-##Chart 4 Drink Barchart
-chart4 = df.query("Category == 'drink'").groupby("Menu")["Price"].agg("sum").round().reset_index()
+# Sort the data.
 chart4_data_sort = chart4.sort_values(by='Price', ascending=True)
 
+# Adjust data label formatting.
 chart4_data_sort["Price_T"] = (chart4_data_sort["Price"]/1000).round(decimals = 1).astype(str) + "K"
 
 
-#setting Chart
+# Create Chart
 chart4_plot = px.bar(chart4_data_sort, 
                     x='Price', 
                     y='Menu', 
@@ -131,27 +136,24 @@ chart4_plot = px.bar(chart4_data_sort,
 
 
 chart4_plot.update_layout(yaxis_title='', xaxis_title = 'Sales') #Rename Axix Title
-#plot horizontal bar chart
 
 
+# Chart 5 Average Sales and Quantity by Day
+# Count unique dates and group by day
+Unique_Day_name = df.groupby("Day Of Week")["Date"]\
+                    .nunique().reset_index(name= "Unique Dates")
 
-###########################################
-
-#Chart 5 
-#Count Date by Day
-Unique_Day_name = df.groupby("Day Of Week")["Date"].nunique().reset_index(name= "Unique Dates")
-
-#Sales Data
+# Count the number of food orders
 sum_sales_unit = df[["Category","Day Of Week"]]\
                     .groupby("Day Of Week")\
                     .agg("count")\
                     .round()\
                     .reset_index()
 
-#Rename Column
+# Rename Column
 sum_sales_unit = sum_sales_unit.rename(columns={"Category": "Total Unit Sales"})
 
-
+# Summarize Total Sales.
 sum_sales = df[["Category","Day Of Week","Price"]]\
             .groupby("Day Of Week")["Price"]\
             .agg("sum")\
@@ -161,38 +163,43 @@ sum_sales = df[["Category","Day Of Week","Price"]]\
 #Rename Column
 sum_sales = sum_sales.rename(columns= {"Price":"Total Sales"})
 
-# Create Sales date with Join unit sales and unique dates table
-sales_data = pd.merge(sum_sales_unit, Unique_Day_name, on="Day Of Week")
-# Create Avg_Unit_sales column
-sales_data["Avg_Unit_sales"] = (sales_data["Total Unit Sales"] / sales_data["Unique Dates"]).round()
-#Join unit sales and unique dates table and overwrite sales data
-sales_data = pd.merge(sum_sales, sales_data, on="Day Of Week")
-# Create Avg_Sales column
-sales_data["Avg_Sales"] = sales_data["Total Sales"] / sales_data["Unique Dates"]
-sales_data["Avg_Sales"] = sales_data["Avg_Sales"].round() #Round Decimal
 
-# Sort the DataFrame based on the order of days of the week
-#สร้าง List day ขึ้มาเพื่อใช้ในการ Sort
+# Merge data
+sales_data = pd.merge(sum_sales_unit, Unique_Day_name, on="Day Of Week")
+
+sales_data = pd.merge(sum_sales, sales_data, on="Day Of Week")
+
+
+# Create new column
+sales_data["Avg_Unit_sales"] = (sales_data["Total Unit Sales"] / sales_data["Unique Dates"]).round()
+
+sales_data["Avg_Sales"] = sales_data["Total Sales"] / sales_data["Unique Dates"]
+
+sales_data["Avg_Sales"] = sales_data["Avg_Sales"].round()
+
+
+
+# Sort the data
 day_order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-#แปลง Day Of Week ให้กลายเป็นข้อมูลประเภท Categorical โดยใช้ day order ในการกำหนดลำดับและจัดเรียง
-sales_data["Day Of Week"] = pd.Categorical(sales_data["Day Of Week"], categories=day_order, ordered=True)
-#เรียงข้อมูล
+
+sales_data["Day Of Week"] = pd.Categorical(sales_data["Day Of Week"], 
+                                            categories=day_order, 
+                                            ordered=True)
+
 sales_data.sort_values("Day Of Week", inplace=True)
 
-#Chart 5 visualize
+# Create Chart 5
 
-###########################################
-# Create subplots with one row and one column
 chart5 = go.Figure()
 
-# Add bar chart for 'Avg Sales' on the primary Y axis
+# Add bar chart
 chart5.add_trace(go.Bar(x=sales_data['Day Of Week'], 
                         y=sales_data['Avg_Sales'], 
                         name='Average Sales', 
                         yaxis='y', 
                         text= sales_data['Avg_Sales']))
 
-# Add line chart for 'Avg Unit sales' on the secondary Y axis
+# Add line chart
 chart5.add_trace(go.Scatter(x=sales_data['Day Of Week'], 
                             y=sales_data['Avg_Unit_sales'], 
                             mode='lines', 
@@ -204,31 +211,27 @@ chart5.add_trace(go.Scatter(x=sales_data['Day Of Week'],
 chart5.update_layout(title='Average Sales and Quantity by Day', 
                      xaxis_title='', 
                      yaxis_title='Sales',
-                     yaxis2=dict(title='Quantity', 
+                     yaxis=dict(showgrid=False, 
+                                range=[0, max(sales_data['Avg_Sales'])+100]),
+                     yaxis2=dict(title='Quantity',
+                                 showgrid=False,
                                  overlaying='y', 
                                  side='right', 
                                  position=1,
-                                 range=[0, max(sales_data['Avg_Unit_sales'])]),  # Set the range to start from 0
+                                 range=[0, max(sales_data['Avg_Unit_sales'])+50]),
                      legend=dict(x=1.05, 
                                  y=1.0, 
                                  xanchor='left', 
                                  yanchor='top'))
 
-chart5.update_layout(yaxis=dict(showgrid=False, zeroline=False))
-# Display the combo chart using Streamlit
 
+# Chart 6 Average Sales and Quantity by Time
 
-
-############################################################
-# Chart 6
 Opened_Day = df["Date"].nunique()
 Unit_by_time = df["Order Hour"]\
             .value_counts()\
             .round()\
             .reset_index()
-
-Unit_by_time = Unit_by_time.rename(columns= {"count":"Sales Unit", "Order Hour": "Hour"})
-
 
 Sales_by_time = df[["Order Hour","Price"]]\
                 .groupby("Order Hour")\
@@ -236,28 +239,32 @@ Sales_by_time = df[["Order Hour","Price"]]\
                 .round()\
                 .reset_index()
 
+# Rename
+Unit_by_time = Unit_by_time.rename(columns= {"count":"Sales Unit", "Order Hour": "Hour"})
 
 Sales_by_time = Sales_by_time.rename(columns= {"Order Hour":"Hour","Price":"Sales"})
 
-
+# Merge data
 time_order = pd.merge(Unit_by_time, Sales_by_time, on="Hour")
+
+# Create new columns
 time_order["Avg Sales Unit"] = time_order["Sales Unit"]/Opened_Day
 time_order["Avg Sales"] = time_order["Sales"]/Opened_Day
+
+# Sort the data
 time_order = time_order.round().sort_values(by= "Hour" )
 
-#Chart 6 Sales by Time
-###################
-# Create subplots with one row and one column
+# Create Chart 6
 chart6 = go.Figure()
 
-# Add bar chart for 'Avg Sales' on the primary Y axis
+# Add bar chart
 chart6.add_trace(go.Bar(x=time_order['Hour'], 
                         y=time_order['Avg Sales'], 
                         name='Average Sales', 
                         yaxis='y', 
                         text= time_order['Avg Sales']))
 
-# Add line chart for 'Avg Unit sales' on the secondary Y axis
+# Add line chart
 chart6.add_trace(go.Scatter(x=time_order['Hour'], 
                             y=time_order['Avg Sales Unit'], 
                             mode='lines', 
@@ -269,27 +276,28 @@ chart6.add_trace(go.Scatter(x=time_order['Hour'],
 chart6.update_layout(title='"Average Sales and Quantity by Time"', 
                     xaxis_title='Hour', 
                     yaxis_title='Sales',
-                    yaxis2=dict(title=' Quantity', 
+                    yaxis=dict(showgrid=False, 
+                               range = [0,max(time_order['Avg Sales']) + 20]),
+                    yaxis2=dict(title=' Quantity',
+                                showgrid=False,
                                 overlaying='y', 
                                 side='right', 
                                 position=1,
-                                range=[0, max(time_order['Avg Sales Unit'])]),  # Set the range to start from 0
+                                range=[0, max(time_order['Avg Sales Unit'])+5]),
                     legend=dict(x=1.05, 
                                 y=1.0, 
                                 xanchor='left', 
                                 yanchor='top'))
 
-chart6.update_layout(yaxis=dict(showgrid=False, zeroline=False))
-# Display the combo chart using Streamlit
 
+#Chart 7 kitchen Manpower
 
-
-#Chart 7 kitchen staff manpower
 fstandard = 10
+
 no_kstaff = df[["Day Of Week","Kitchen Staff"]]\
-    .groupby("Day Of Week")\
-    .agg("mean")\
-    .reset_index().round()
+                .groupby("Day Of Week")\
+                .agg("mean")\
+                .reset_index().round()
 
 df_kstaff = df[df["Category"] == "food"]\
                 .groupby(["Day Of Week"])["Waiting Time"]\
@@ -297,26 +305,26 @@ df_kstaff = df[df["Category"] == "food"]\
                 .reset_index()\
                 .round()
 
+# Merge data
 df_kstaff = pd.merge(df_kstaff, no_kstaff, on="Day Of Week")
 
-
-df_kstaff["Day Of Week"] = pd.Categorical(df_kstaff["Day Of Week"], 
-    categories=day_order, ordered=True)
+# Sort data
+df_kstaff["Day Of Week"] = pd.Categorical(df_kstaff["Day Of Week"],categories=day_order, ordered=True)
 
 df_kstaff.sort_values("Day Of Week", inplace=True)
 
-###################
-# Create subplots with one row and one column
+
+# Create Chart 7
 chart7 = go.Figure()
 
-# Add bar chart for 'Avg Sales' on the primary Y axis
+# Add bar chart
 chart7.add_trace(go.Bar(x=df_kstaff['Day Of Week'], 
                         y=df_kstaff['Kitchen Staff'], 
                         name='Avg Staff', 
                         yaxis='y', 
                         text= df_kstaff['Kitchen Staff']))
 
-# Add line chart for 'Avg Unit sales' on the secondary Y axis
+# Add line chart
 chart7.add_trace(go.Scatter(x=df_kstaff['Day Of Week'], 
                             y=df_kstaff['Waiting Time'], 
                             mode='lines', 
@@ -324,37 +332,35 @@ chart7.add_trace(go.Scatter(x=df_kstaff['Day Of Week'],
                             yaxis='y2',
                             line=dict(color='red')))
 
-# Add constant line with name 'Standard Time'
+# Add constant line
 chart7.add_trace(go.Scatter(x=df_kstaff['Day Of Week'], 
                             y=[fstandard] * len(df_kstaff),  # Create a list of 10s with the same length as the data
                             mode='lines', 
-                            name='Standard Time', 
+                            name='Target Time', 
                             yaxis='y2',
                             line=dict(color='yellow', width=2, dash='dashdot')))
 
 # Update layout
 chart7.update_layout(title='Kitchen Manpower', 
-                    xaxis_title='', 
-                    yaxis_title='Kitchen Staff',
-                    yaxis2=dict(title='Waiting Time (Minutes)', 
-                                overlaying='y', 
-                                side='right', 
-                                position=1,
-                                range=[0, max(df_kstaff['Waiting Time'])]),  # Set the range to start from 0
-                    legend=dict(x=1.05, 
-                                y=1.0, 
-                                xanchor='left', 
-                                yanchor='top'))
-
-# Update layout to remove grid lines and zero lines for y axis
-chart7.update_layout(yaxis=dict(showgrid=False, zeroline=False))
-
-# Display the combo chart using Streamlit
+                     xaxis_title='', 
+                     yaxis_title='Kitchen Staff',
+                     yaxis=dict(range=[0, max(df_kstaff['Kitchen Staff']) + 5],
+                                showgrid=False),  
+                     yaxis2=dict(title='Waiting Time (Minutes)',
+                                 showgrid=False,
+                                 overlaying='y', 
+                                 side='right', 
+                                 position=1,
+                                 range=[0, max(df_kstaff['Waiting Time']) + 10]),
+                     legend=dict(x=1.05, 
+                                 y=1.0, 
+                                 xanchor='left', 
+                                 yanchor='top'))
 
 
-
-##Chart 8 Drink staff manpower
+##Chart 8 Drinks Manpower
 dstandard = 5
+
 no_dstaff = df[["Day Of Week","Drinks Staff"]]\
         .groupby("Day Of Week")\
         .agg("mean").reset_index().round()
@@ -365,23 +371,28 @@ df_dstaff = df[df["Category"] == "drink"]\
                 .reset_index()\
                 .round()
 
+# Merge data
 df_dstaff = pd.merge(df_dstaff, no_dstaff, on="Day Of Week")
+
+# Sort data
 df_dstaff["Day Of Week"] = pd.Categorical(df_dstaff["Day Of Week"], categories=day_order, ordered=True)
+
 df_dstaff.sort_values("Day Of Week", inplace=True)
 
 
-###################
-# Create subplots with one row and one column
+
+# Create Chart 8
+
 chart8 = go.Figure()
 
-# Add bar chart for 'Avg Sales' on the primary Y axis
+# Add bar chart
 chart8.add_trace(go.Bar(x=df_dstaff['Day Of Week'], 
                         y=df_dstaff['Drinks Staff'], 
                         name='Avg Staff', 
                         yaxis='y', 
                         text= df_dstaff['Drinks Staff']))
 
-# Add line chart for 'Avg Unit sales' on the secondary Y axis
+# Add line chart
 chart8.add_trace(go.Scatter(x=df_dstaff['Day Of Week'], 
                             y=df_dstaff['Waiting Time'], 
                             mode='lines', 
@@ -389,7 +400,7 @@ chart8.add_trace(go.Scatter(x=df_dstaff['Day Of Week'],
                             yaxis='y2',
                             line=dict(color='red')))
 
-# Add constant line with name 'Standard Time'
+# Add constant line
 chart8.add_trace(go.Scatter(x=df_dstaff['Day Of Week'], 
                             y=[dstandard] * len(df_dstaff),  # Create a list of 10s with the same length as the data
                             mode='lines', 
@@ -401,24 +412,24 @@ chart8.add_trace(go.Scatter(x=df_dstaff['Day Of Week'],
 chart8.update_layout(title='Drinks Manpower', 
                     xaxis_title='', 
                     yaxis_title='Drink Staff',
-                    yaxis2=dict(title='Waiting Time (Minutes)', 
+                    yaxis=dict(showgrid = False,
+                                range=[0, max(df_dstaff['Drinks Staff'])+5]),
+                    yaxis2=dict(title='Waiting Time (Minutes)',
+                                showgrid = False, 
                                 overlaying='y', 
                                 side='right', 
                                 position=1,
-                                range=[0, max(df_dstaff['Waiting Time'])]),  # Set the range to start from 0
+                                range=[0, max(df_dstaff['Waiting Time'])+10]),  # Set the range to start from 0
                     legend=dict(x=1.05, 
                                 y=1.0, 
                                 xanchor='left', 
                                 yanchor='top'))
 
-# Update layout to remove grid lines and zero lines for y axis
-chart8.update_layout(yaxis=dict(showgrid=False, zeroline=False))
 
 
-
-
-
+# Set up Page layout
 col1, col2 = st.columns(2)
+
 # Display Chart 1-4 in the first column
 with col1:
     st.plotly_chart(chart1, use_container_width=True)
